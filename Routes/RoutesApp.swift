f49e02aa73f42aba0fee8961a98b0a16a0c32137
Blake Haug <blake@blakehaug.com>
2024-01-02 14:54:56 -0800

    Initial Commit

---
+++ Routes/RoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutesApp.swift
+//  Routes
+//
+//  Created by Blake Haug on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
